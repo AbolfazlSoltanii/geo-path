db38db2ae6cffc0e96a0603cef06fdc933be5311
Soli <soli@gmail.com>
2026-01-29 20:40:00 +0330

    init : initialize geo path project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {type FC} from "react";
+
+const App: FC = () => {
+    return (
+        <div className="relative w-full h-screen">
+            <div className="text-2xl p-4 z-10 relative">
+                Welcome into GeoPath
+            </div>
+        </div>
+    );
+};
+
+export default App;
